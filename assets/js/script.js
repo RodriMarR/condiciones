d78d66b68a47c187b493146fe49d18ec4ef48af8
RodriMarR <rodrigo.martinez.r@usach.cl>
2024-06-13 19:46:41 -0400

    agregar archivos

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function bordeImagen() {
+    if (document.getElementById("imagenParte1").style.border == "2px solid red") {
+        document.getElementById("imagenParte1").style.border = "none";
+        
+    } else {
+        document.getElementById("imagenParte1").style.border = "2px solid red";
+    }
+
+}
+
+function calculo() {
+    let valor1 = Number(document.getElementById("cantidadIn1").value);
+    let valor2 = Number(document.getElementById("cantidadIn2").value);
+    let valor3 = Number(document.getElementById("cantidadIn3").value);
+    let mensaje = document.getElementById("mensaje");
+    let total = valor1+valor2+valor3;
+    if (total<=10) {
+        mensaje.innerHTML = "Llevas: "+total+" Quacks";
+    }else{
+        mensaje.innerHTML = "¡Ja! Parece ser que esta compra solo resiste 10 Quacks.";
+    }
+}
+function validacion() {
+    let valor1 = document.getElementById("select1").value;
+    let valor2 = document.getElementById("select2").value;
+    let valor3 = document.getElementById("select3").value;
+    let mensajePassword = document.getElementById("mensajePassword");
+
+    if (valor1==9 && valor2==1 && valor3==1) {
+        mensajePassword.innerHTML = "password 1 correcto";
+    } else if(valor1==7 && valor2==1 && valor3==4){
+        mensajePassword.innerHTML = "password 2 es correcto";
+    }else{
+        mensajePassword.innerHTML = "password incorrecto";
+    }
+}
